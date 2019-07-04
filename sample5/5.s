cf268df7cb56789da53745e687fac1acca0eed1c
Goki Sakuraba <goki14914@icloud.com>
2019-07-04 21:12:52 +0900

    init commit

---
+++ sample5/5.s
@@ -0,0 +1,36 @@
+	.file	1 "5.c"
+	.section .mdebug.abi32
+	.previous
+	.abicalls
+	.text
+	.align	2
+	.globl	main
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,24,$31		# vars= 8, regs= 1/0, args= 0, gp= 8
+	.mask	0x40000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	
+	addiu	$sp,$sp,-24
+	sw	$fp,16($sp)
+	move	$fp,$sp
+	li	$2,2			# 0x2
+	sw	$2,8($fp)
+	lw	$2,8($fp)
+	nop
+	addiu	$2,$2,1
+	sw	$2,8($fp)
+	move	$2,$0
+	move	$sp,$fp
+	lw	$fp,16($sp)
+	addiu	$sp,$sp,24
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.ident	"GCC: (GNU) 4.2.4"
