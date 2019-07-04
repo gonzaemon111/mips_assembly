a2c03d553d771fdfbb55df956482b980aa296397
Goki Sakuraba <goki14914@icloud.com>
2019-07-04 21:05:33 +0900

    add sample3

---
+++ sample3/3.s
@@ -0,0 +1,30 @@
+	.file	1 "3.c"
+	.section .mdebug.abi32
+	.previous
+	.abicalls
+	.text
+	.align	2
+	.globl	main
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,24,$31		# vars= 8, regs= 1/0, args= 0, gp= 8
+	.mask	0x40000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	
+	addiu	$sp,$sp,-24
+	sw	$fp,16($sp)
+	move	$fp,$sp
+	move	$2,$0
+	move	$sp,$fp
+	lw	$fp,16($sp)
+	addiu	$sp,$sp,24
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.ident	"GCC: (GNU) 4.2.4"
